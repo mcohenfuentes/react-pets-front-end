fa176ce0722f2c35d5ea8713cd89dde57f2260f9
Mica CF <mcohenfuentes@gmail.com>
2025-03-12 12:27:09 -0400

    end and petlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import * as petService from './services/petService';
+import PetList from './components/PetList/PetList';
 import './App.css'
 
 const App = () => {
-  return <h1>Hello world!</h1>;
-};
+  const [pets, setPets] = useState([]);
 
+  
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const fetchedPets = await petService.index();
+        // Don't forget to pass the error object to the new Error
+        if (fetchedPets.err) {
+          throw new Error(fetchedPets.err);
+        }
+        setPets(fetchedPets);
+      } catch (err) {
+        // Log the error object
+        console.log(err);
+      }
+    };
+    fetchPets();
+  }, []);
+
+  return (
+    <>
+      <PetList pets={pets} />
+    </>
+  );
+};
 export default App;
